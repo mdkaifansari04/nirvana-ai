496b2e5528c93687fb978eec57dd8337e3619541
Md Kaif Ansari <amdkaif843@gmail.com>
2025-04-06 17:30:32 +0000

    chore: udpate app metadata
    Signed-off-by: Md Kaif Ansari <amdkaif843@gmail.com>

---
+++ frontend/constants/index.ts
@@ -0,0 +1,4 @@
+export const APP_NAME = 'Nirvana-Ai' as const;
+
+export const APP_DESCRIPTION =
+   'Niravana AI is a revolutionary app that blends mental health support with personalized learning resources, designed specifically for Indian students battling stress, academic pressure, and emotional isolation.' as const;

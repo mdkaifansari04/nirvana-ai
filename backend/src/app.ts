a4637ce0fdbfe59bacbe4de3311b275ccaa8ed79
Md Kaif Ansari <amdkaif843@gmail.com>
2025-04-13 10:45:56 +0000

    feat: add clerk auth
    Signed-off-by: Md Kaif Ansari <amdkaif843@gmail.com>

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,32 +1,34 @@
-import { PORT } from './config/env';
+import { PORT } from "./config/env";
 
-import express, { type Request, type Response } from 'express';
-import errorHandler from './middleware/error';
-import router from './api/v1/routes';
-import morgan from 'morgan';
-import connectDB from './lib/mongoose';
-import { clerkMiddleware, requireAuth } from '@clerk/express';
+import express, { type Request, type Response } from "express";
+import errorHandler from "./middleware/error";
+import router from "./api/v1/routes";
+import morgan from "morgan";
+import connectDB from "./lib/mongoose";
+import { clerkMiddleware, getAuth, requireAuth } from "@clerk/express";
+import { verifyUser } from "./middleware/authorization";
 
 connectDB();
 
 const app = express();
 app.use(clerkMiddleware());
+app.use(requireAuth());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
-app.get('/health', (req: Request, res: Response) => {
-   res.status(200).json({
-      status: 'OK',
-      timestamp: new Date().toISOString(),
-   });
+app.get("/health", (req: Request, res: Response) => {
+  res.status(200).json({
+    status: "OK",
+    timestamp: new Date().toISOString(),
+  });
 });
 // app.use(requireAuth());
-app.use('/api/v1', router);
+app.use("/api/v1", router);
 
 app.use(errorHandler);
 
 app.listen(PORT, () => {
-   console.log(`Server is running at http://localhost:${PORT}`);
+  console.log(`Server is running at http://localhost:${PORT}`);
 });

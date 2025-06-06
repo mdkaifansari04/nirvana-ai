80147dc53e3c415a1cf724d310ac73bc0847ba09
Md Kaif Ansari <amdkaif843@gmail.com>
2025-06-06 19:54:16 +0530

    feat(layout): add tracking script to RootLayout and format code for consistency

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,41 +1,47 @@
-import type { Metadata } from 'next';
-import { Urbanist } from 'next/font/google';
-import './globals.css';
-import { Toaster } from '@/components/shared/toaster';
-import { APP_DESCRIPTION, APP_NAME, CLERK_TEMPLATE_NAME } from '@/constants';
-import { ClientProvider } from '@/provider/client-provider';
-import { ClerkTokenProvider } from '@/provider/token-provider';
-import { ClerkProvider } from '@clerk/nextjs';
+import type { Metadata } from "next";
+import { Urbanist } from "next/font/google";
+import "./globals.css";
+import { Toaster } from "@/components/shared/toaster";
+import { APP_DESCRIPTION, APP_NAME, CLERK_TEMPLATE_NAME } from "@/constants";
+import { ClientProvider } from "@/provider/client-provider";
+import { ClerkTokenProvider } from "@/provider/token-provider";
+import { ClerkProvider } from "@clerk/nextjs";
+import Script from "next/script";
 
 const urbanist = Urbanist({
-   subsets: ['latin'],
-   weight: ['400', '500', '600'],
+  subsets: ["latin"],
+  weight: ["400", "500", "600"],
 });
 
 export const metadata: Metadata = {
-   title: APP_NAME,
-   description: APP_DESCRIPTION,
+  title: APP_NAME,
+  description: APP_DESCRIPTION,
 };
 
 export default function RootLayout({
-   children,
+  children,
 }: Readonly<{
-   children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-   return (
-      <html lang="en" suppressHydrationWarning>
-         <body className={`${urbanist.className} scroll-smooth antialiased bg-[#F7F4F2]`}>
-            <ClerkProvider>
-               <ClientProvider>
-                  <ClerkTokenProvider templateName={CLERK_TEMPLATE_NAME}>
-                     <main>
-                        {children}
-                        <Toaster />
-                     </main>
-                  </ClerkTokenProvider>
-               </ClientProvider>
-            </ClerkProvider>
-         </body>
-      </html>
-   );
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${urbanist.className} scroll-smooth antialiased bg-[#F7F4F2]`}>
+        <ClerkProvider>
+          <ClientProvider>
+            <ClerkTokenProvider templateName={CLERK_TEMPLATE_NAME}>
+              <main>
+                {children}
+                <Toaster />
+              </main>
+              <Script
+                defer
+                data-domain="www.nirwana-ai.vercel.app"
+                src="https://getanalyzr.vercel.app/tracking-script.js"
+              />
+            </ClerkTokenProvider>
+          </ClientProvider>
+        </ClerkProvider>
+      </body>
+    </html>
+  );
 }
